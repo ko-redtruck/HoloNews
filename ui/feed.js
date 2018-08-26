1ca4371141f3e020b1c76cdd7a00bc52d43185d2
ko-redtruck <ketterko@gmail.com>
2018-08-26 19:52:03 +0200

    images are uploaded to ipfs instead of the public Holochain DHT

--- ui/feed.js
+++ ui/feed.js
@@ -4,7 +4,10 @@ var data = {
   //elements of the post upload
   postBody:"",
   postTitle: "",
+  //image src for displaying a preview
   postImageSrc: "",
+  //data for ipfs upload
+  postImageBuffer: "",
   //post array containing all uploaded posts
   posts:[]
 }
@@ -35,18 +38,46 @@ var vm = new Vue({
   }
 })
 
+//ipfs node setup
+const node = new Ipfs({ repo: "ipfs-" + Math.random});
+
+node.on("ready", async function () {
+  //wait for the IPFS version to debug
+  const version = await node.version();
+  console.log("Ipfs-Version: " + version.version);
+})
+
 function uploadPost() {
-  //post schema
+  //firstly uploads all images and videos to IPFS
+  //then commits the rest to the public DHT
+  //configure all the data that will be commited to the DHT
+  //Holochain post schema
   var postData = {
     //data.postBody == vm.postBody
     "body": data.postBody,
     "title": data.postTitle,
-    "imageSrc": data.postImageSrc
+    "postImageHashes": []
   }
 
-  //call the post function in the zome "posting"
-  request("posting","post",JSON.stringify(postData),function (result) {
+  node.files.add(new node.types.Buffer(data.postImageBuffer), async function(err, filesAdded) {
+    if (err) {
+      return console.error('Error - ipfs add', err, res)
+    }
+
+    await filesAdded.forEach(function (file) {
+      console.log('successfully stored', file.hash)
+      postData.postImageHashes.push(file.hash)
+    })
+
+    //call the post function in the zome "posting"
+    request("posting","post",JSON.stringify(postData),function (result) {
+      //callback
+    })
+
   })
+
+
+
 }
 
 function loadPosts() {
@@ -56,25 +87,22 @@ function loadPosts() {
 }
 
 function onFileChanged(event) {
-  return createImage(event.target.files[0])
+  return createBuffer(event.target.files[0])
 }
 
 
 
 //creates/ reads image src from file
-function createImage(file) {
+function createBuffer(file) {
   var reader = new FileReader();
+  var enc = new TextEncoder(); // always utf-8
+  var dec = new TextDecoder("utf-8");
 
   reader.onloadend = function () {
-    data.postImageSrc =  reader.result;
+    //convert it to a buffer for ipfs to upload
+    data.postImageBuffer = JSON.stringify(Buffer.from(reader.result));
+    //convert it to base64 to display it as an image.src
+    data.postImageSrc = base64ArrayBuffer(reader.result);
   }
-  reader.readAsDataURL(file);
+  reader.readAsArrayBuffer(file);
 }
-
-const node = new Ipfs({ repo: 'ipfs-' + Math.random() })
-    node.once('ready', () => {
-      console.log('Online status: ', node.isOnline() ? 'online' : 'offline')
-      // You can write more code here to use it. Use methods like
-      // node.files.add, node.files.get. See the API docs here:
-      // https://github.com/ipfs/interface-ipfs-core
-    })
