13b608af33a6e22386314e92cb8086c65466ea0a
ko-redtruck <ketterko@gmail.com>
2018-08-27 23:37:03 +0200

    code clean up

--- ui/feed.js
+++ ui/feed.js
@@ -7,8 +7,6 @@ var data = {
   postImageHashes: [],
   //image src for displaying a preview
   postImageSrc: "",
-  //data for ipfs upload
-  postImageBuffer: "",
   //post array containing all uploaded posts
   posts:[]
 }
@@ -18,7 +16,7 @@ Vue.component("post-feed",{
     post: {
       type: Object,
       required: true
-    }
+    }x
   },
   template: `
     <div class="post-window">
@@ -40,11 +38,6 @@ Vue.component("post-feed",{
   }
 })
 
-function testF() {
-  console.log("function executed dynamically");
-  return "testF-id"
-}
-
 var vm = new Vue({
   el: "#app",
   data: data,
@@ -55,18 +48,6 @@ var vm = new Vue({
   }
 })
 
-/*
-//ipfs node setup for ipfs-js
-const node = new Ipfs({ repo: "ipfs-" + Math.random});
-
-node.on("ready", async function () {
-  //wait for the IPFS version to debug
-  const version = await node.version();
-  console.log("Ipfs-Version: " + version.version);
-})
-*/
-
-
 if (window.ipfs) {
   console.log("window.ipfs is active")
   const node = window.ipfs;
@@ -75,71 +56,6 @@ if (window.ipfs) {
 } else {
     // Fallback
     console.log("no ipfs browser plugin found")
-}
-
-
-
-
-function getImageSrc(hash,componentInstance) {
-  //this === vue component instance
-  console.log(hash)
-  console.log(componentInstance)
-
-  node.cat(hash, function (err, file,componentInstance) {
-    if (err) {
-      throw err
-    }
-    else {
-      console.log(file.toString('utf8'))
-
-      var arrayBufferView = new Uint8Array( file );
-      var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
-      var reader = new FileReader();
-      reader.onloadend = function(componentInstance) {
-        var base64data = reader.result;
-        console.log(base64data);
-        console.log(typeof base64data);
-        this.imageSrc= "base64data";
-      }
-      reader.readAsDataURL(blob);
-
-      /*
-      var urlCreator = window.URL || window.webkitURL;
-      console.log(urlCreator.createObjectURL( blob ))
-      var ObjectURL =  urlCreator.createObjectURL( blob )
-
-      var img = document.createElement("img");
-      img.src = ObjectURL
-      img.onload = function() {
-        urlCreator.revokeObjectURL(this.src);
-      }
-      document.querySelector(".post-window").append(img);
-      console.log(img)
-      */
-    }
-  })
-
-  /*
-  node.cat(this.post.Entry.postImageHashes[0]).then((stream) => {
-      var buf = [];
-      var blob;
-      stream.on('data', (file) => {
-        var data = Array.prototype.slice.call(file);
-        buf = buf.concat(data);
-      });
-      stream.on('end', (file) => {
-        if (typeof blob !== 'undefined') {
-          window.URL.revokeObjectURL(blob);
-        }
-
-        buf = new Buffer(buf);
-        blob = new Blob([buf], { type: 'image/jpg' });
-        console.log(blob)
-        console.log(window.URL.createObjectURL(blob))
-        return window.URL.createObjectURL(blob);
-      });
-    });
-    */
 }
 
 function uploadPost() {
@@ -153,21 +69,10 @@ function uploadPost() {
     "title": data.postTitle,
     "postImageHashes": data.postImageHashes
   }
-  /*
-  node.add(data.postImageBuffer, async function(err, filesAdded) {
-    if (err) {
-      return console.error('Error - ipfs add', err)
-    }
-
-    await filesAdded.forEach(function (file) {
-      console.log('successfully stored', file.hash)
-      postData.postImageHashes.push(file.hash)
-    })
-    */
-    //call the post function in the zome "posting"
-    request("posting","post",JSON.stringify(postData),function (result) {
-      //callback
-    })
+  //call the post function in the zome "posting"
+  request("posting","post",JSON.stringify(postData),function (result) {
+    //callback
+  })
 }
 
 
@@ -178,19 +83,13 @@ function loadPosts() {
 }
 
 function onFileChanged(event) {
-  return createBuffer(event.target.files[0])
+  return uploadToIpfs(event.target.files[0])
 }
 
-
-
-//creates/ reads image src from file
-function createBuffer(file) {
+function uploadToIpfs(file) {
   var reader = new FileReader();
-  var enc = new TextEncoder(); // always utf-8
-  var dec = new TextDecoder("utf-8");
 
   reader.onloadend = function () {
-    //convert it to a buffer for ipfs to upload
     window.ipfs.add(Buffer.from(reader.result), async function(err, filesAdded) {
       if (err) {
         return console.error('Error - ipfs add', err)
